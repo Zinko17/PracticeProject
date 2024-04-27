8d154921fa51505a5353092d5795c77eed977fb3
Zinko17 <vovazin2@gmail.com>
2024-04-27 20:58:05 +0600

    some templates for main page

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Service
 
 # Create your views here.
 
-def main():
-    pass
+def main(request):
+    service = Service.objects.all()
+    return render(request, 'mainapp/main.html', {'service': service})
